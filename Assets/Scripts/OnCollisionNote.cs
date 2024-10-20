c6d5ced169c2a3272339c0ccd0fd98c395423246
jorgemr3 <a2213332183@alumnos.uat.edu.mx>
2024-10-19 23:58:45 -0600

    sonidos de punto y fail y de fondo, no detecta el punto por alguna razon

--- Assets/Scripts/OnCollisionNote.cs
+++ Assets/Scripts/OnCollisionNote.cs
@@ -7,30 +7,46 @@ public class OnCollisionNote : MonoBehaviour
     NotesController notesController;
     TextMeshProUGUI textCounterPoints;
 
+    AudioSource punto;
+
+    AudioSource fail;
+
     void Start()
     {
         notesController = NotesController.Instance;
-
-        textCounterPoints = GameObject.Find("CounterPoints").GetComponent<TextMeshProUGUI>();
-        textCounterPoints.text = notesController.GetPoints().ToString();
+        punto = GameObject.Find("Point").GetComponent<AudioSource>();
+        fail = GameObject.Find("Fail").GetComponent<AudioSource>();
+        //textCounterPoints = GameObject.Find("CounterPoints").GetComponent<TextMeshProUGUI>();
+        // textCounterPoints.text = notesController.GetPoints().ToString();
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.tag == "Player")
+        Debug.Log("colision detectada");
+        if (other.gameObject.CompareTag("Player")) //no detecta el punto
         {
+            Debug.Log("Puntote");
+            punto.PlayOneShot(punto.clip);
             notesController.AddPoint();
             gameObject.SetActive(false);
         }
         else
-        {
+        { 
+            Debug.Log("No Punto");
+            fail.PlayOneShot(fail.clip);
             notesController.SubtractPoint();
         }
 
-        textCounterPoints.text = notesController.GetPoints().ToString();
+        //textCounterPoints.text = notesController.GetPoints().ToString();
 
         gameObject.SetActive(false);
     }
+
+    // void Reproduce(AudioClip clip)
+    // {
+    //     audioSource.clip = clip;
+    //     audioSource.Play();
+    // }
 
 
 }
